d4770e884fd915829db8049fed0808be62eb08e7
Francois Lannoy <flannoy154@gmail.com>
2021-01-31 12:13:58 +0100

    :twisted_rightwards_arrows: merged main into dev

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
 const addition = (a, b) => a+b
 
 console.log(addition(1, 2))
+
+const multiplication = (a, b) => a*b
+
+console.log(multiplication(1, 2))
+
+const division = (a, b) => a*b
+
+console.log(division(1, 2))
